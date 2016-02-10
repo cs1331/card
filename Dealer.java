a37c4400efcdc480fed956b3ec69243fe90a341f
Chris Simpkins <chris.simpkins@gmail.com>
2016-02-10 12:54:54 -0500

    Add Dealer class

---
+++ Dealer.java
@@ -0,0 +1,9 @@
+public class Dealer {
+
+    public static void main(String[] args) {
+        Card c = new Card();
+        c.rank = "ace";
+        c.suit = "base";
+        System.out.println(c);
+    }
+}
