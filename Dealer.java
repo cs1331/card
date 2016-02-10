5c779105bca3d53d49cf42a8c472f142c19bc40c
Chris Simpkins <chris.simpkins@gmail.com>
2016-02-10 13:10:16 -0500

    Add printing of uninitialized Card

--- Dealer.java
+++ Dealer.java
@@ -2,6 +2,7 @@ public class Dealer {
 
     public static void main(String[] args) {
         Card c = new Card();
+        System.out.println(c); // Printing a new Card instance
         c.setRank("ace");
         c.setSuit("spades");
         System.out.println(c);
