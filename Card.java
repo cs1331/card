19d6fcee80963057fc7e933df6114f9cca4264a9
Chris Simpkins <chris.simpkins@gmail.com>
2016-02-10 12:52:14 -0500

    Add toString() method

--- Card.java
+++ Card.java
@@ -3,8 +3,18 @@ public class Card {
     String rank;
     String suit;
 
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
     public static void main(String[] args) {
-        Card c = new Card();
-        System.out.println(c);
+        Card swedishPop = new Card();
+        swedishPop.rank = "ace";
+        swedishPop.suit = "base";
+        Card handy = new Card();
+        handy.rank = "jack";
+        handy.suit = "all trades";
+        System.out.println(swedishPop);
+        System.out.println(handy);
     }
 }
