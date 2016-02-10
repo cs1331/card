ff3dbdb4eea61ed41a0bb193191ddfb0202d5c5a
Chris Simpkins <chris.simpkins@gmail.com>
2016-02-10 13:03:48 -0500

    Enforce valid rank and suit in Card setters

--- Card.java
+++ Card.java
@@ -1,13 +1,46 @@
 public class Card {
+
+    private final String[] VALID_RANKS =
+        {"2", "3", "4", "5", "6", "7", "8", "9",
+         "10", "jack", "queen", "king", "ace"};
+
+    private final String[] VALID_SUITS =
+        {"diamonds", "clubs", "hearts","spades"};
+
     private String rank;
     private String suit;
 
     public void setRank(String rank) {
+        if (!isValidRank(rank)) {
+            System.out.println(rank + " is not a valid rank.");
+            System.exit(0);
+        }
         this.rank = rank;
     }
 
+    private boolean isValidRank(String someRank) {
+        return contains(VALID_RANKS, someRank);
+    }
+
     public void setSuit(String suit) {
+        if (!isValidSuit(suit)) {
+            System.out.println(suit + " is not a valid suit.");
+            System.exit(0);
+        }
         this.suit = suit;
+    }
+
+    private boolean isValidSuit(String someSuit) {
+        return contains(VALID_SUITS, someSuit);
+    }
+
+    private boolean contains(String[] array, String item) {
+        for (String element: array) {
+            if (element.equals(item)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public String toString() {
